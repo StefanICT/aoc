ddb75cc837782bc91f6a24d1d81b9ef87c1a8d87
Stefan van der Wolf <stefan@dexels.com>
2021-12-02 22:32:03 +0100

    Solve day 2

---
+++ 2/main.py
@@ -0,0 +1,33 @@
+with open("2/commands1.1") as fp:
+    horizontal = 0
+    depth = 0
+
+    for line in fp.read().splitlines():
+        action, n = line.split(" ")
+
+        if action == "forward":
+            horizontal += int(n)
+        elif action == "down":
+            depth += int(n)
+        elif action == "up":
+            depth -= int(n)
+
+    print(f"Day 2.1: {horizontal * depth}")
+
+with open("2/commands1.2") as fp:
+    horizontal = 0
+    depth = 0
+    aim = 0
+
+    for line in fp.read().splitlines():
+        action, n = line.split(" ")
+
+        if action == "forward":
+            horizontal += int(n)
+            depth += aim * int(n)
+        elif action == "down":
+            aim += int(n)
+        elif action == "up":
+            aim -= int(n)
+
+    print(f"Day 2.2: {horizontal * depth}")
