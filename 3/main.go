cda8fef2683aa316fac2ff8d99018ace0d5fefd6
Stefan van der Wolf <stefan@dexels.com>
2021-12-05 00:50:35 +0100

    Solve day 3.2

--- 3/main.go
+++ 3/main.go
@@ -3,12 +3,14 @@ package main
 import (
 	"bufio"
 	"fmt"
+	"math"
 	"os"
 	"strconv"
 )
 
 func main() {
-    part1()
+	part1()
+	part2()
 }
 
 func part1() {
@@ -41,5 +43,64 @@ func part1() {
 		}
 	}
 
-	fmt.Printf("Day 3: %d\n", gamma*epsilon)
+	fmt.Printf("Day 3.1: %d\n", gamma*epsilon)
+}
+
+func part2() {
+	file, _ := os.Open("3/report")
+	defer file.Close()
+
+	length := 12
+	numbers := make([]uint64, 0)
+
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		n, _ := strconv.ParseUint(scanner.Text(), 2, 64)
+		numbers = append(numbers, n)
+	}
+
+	oxygens := numbers
+	co2s := numbers
+
+	for i := length - 1; i >= 0; i-- {
+		if len(oxygens) > 1 {
+			target := uint8(0)
+			if float64(common(oxygens, i)) >= math.Round(float64(len(oxygens))/2.0) {
+				target = 1
+			}
+
+			oxygens = filter(oxygens, i, target)
+		}
+
+		if len(co2s) > 1 {
+			target := uint8(1)
+			if float64(common(co2s, i)) >= math.Round(float64(len(co2s))/2.0) {
+				target = 0
+			}
+
+			co2s = filter(co2s, i, target)
+		}
+	}
+
+	fmt.Println("Day 3.2:", oxygens[0]*co2s[0])
+}
+
+func common(numbers []uint64, i int) uint64 {
+	n := uint64(0)
+	for _, number := range numbers {
+		n += number >> i & 1
+	}
+	return n
+}
+
+func filter(numbers []uint64, i int, target uint8) []uint64 {
+	slice := []uint64{}
+	for _, n := range numbers {
+		if uint8(n>>i&1) == target {
+			slice = append(slice, n)
+		}
+
+	}
+
+	return slice
 }
