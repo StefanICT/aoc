3d0a99acac4d32d46a003a2d6034addc3cf6cc7b
Stefan van der Wolf <stefan@dexels.com>
2021-12-06 22:16:26 +0100

    Cleanup

--- 2015/3/main.go
+++ 2015/3/main.go
@@ -1,20 +1,70 @@
 package main
 
 import (
-    "fmt"
+	. "aoc/util"
+	"fmt"
 )
 
+type Point struct {
+	x int
+	y int
+}
+
 func main() {
-    part1()
-    part2()
+	part1()
+	part2()
 }
 
 func part1() {
-    for _, line := range ReadLines("2015/3/input") {
-
-    }
+	current := Point{0, 0}
+	data := map[Point]int{
+		current: 1,
+	}
+	for _, line := range ReadLines("2015/3/input") {
+		for i := 0; i < len(line); i++ {
+			switch string(line[i]) {
+			case "^":
+				current.y -= 1
+			case ">":
+				current.x += 1
+			case "v":
+				current.y += 1
+			case "<":
+				current.x -= 1
+			}
+			data[current] = data[current] + 1
+		}
+	}
+	fmt.Println("Day 3.1", len(data))
 }
 
 func part2() {
+	santa := Point{0, 0}
+	robot := Point{0, 0}
+	current := &santa
 
+	data := map[Point]int{
+		*current: 1,
+	}
+	for _, line := range ReadLines("2015/3/input") {
+		for i := 0; i < len(line); i++ {
+			switch string(line[i]) {
+			case "^":
+				(*current).y -= 1
+			case ">":
+				(*current).x += 1
+			case "v":
+				(*current).y += 1
+			case "<":
+				(*current).x -= 1
+			}
+			data[*current] = data[*current] + 1
+			if current == &santa {
+				current = &robot
+			} else {
+				current = &santa
+			}
+		}
+	}
+	fmt.Println("Day 3.2", len(data))
 }
--- 2021/2/main.go
+++ 2021/2/main.go
@@ -3,7 +3,7 @@ package main
 import (
 	. "aoc/util"
 	"fmt"
-    "strings"
+	"strings"
 )
 
 func main() {
@@ -16,43 +16,43 @@ func part1() {
 	depth := 0
 
 	for _, line := range ReadLines("2021/2/input") {
-        components := strings.Split(line, " ")
-        action := components[0]
-        number := AsInt(components[1])
+		components := strings.Split(line, " ")
+		action := components[0]
+		number := AsInt(components[1])
 
-        switch action {
-        case "forward":
-            horizontal += number
-        case "down":
-            depth += number
-        case "up":
-            depth -= number
-        }
+		switch action {
+		case "forward":
+			horizontal += number
+		case "down":
+			depth += number
+		case "up":
+			depth -= number
+		}
 	}
 
-	fmt.Printf("Day 2.1: %d\n", horizontal * depth)
+	fmt.Printf("Day 2.1: %d\n", horizontal*depth)
 }
 
 func part2() {
 	horizontal := 0
 	depth := 0
-    aim := 0
+	aim := 0
 
 	for _, line := range ReadLines("2021/2/input") {
-        components := strings.Split(line, " ")
-        action := components[0]
-        number := AsInt(components[1])
+		components := strings.Split(line, " ")
+		action := components[0]
+		number := AsInt(components[1])
 
-        switch action {
-        case "forward":
-            horizontal += number
-            depth += aim * number
-        case "down":
-            aim += number
-        case "up":
-            aim -= number
-        }
+		switch action {
+		case "forward":
+			horizontal += number
+			depth += aim * number
+		case "down":
+			aim += number
+		case "up":
+			aim -= number
+		}
 	}
 
-	fmt.Printf("Day 2.2: %d\n", horizontal * depth)
+	fmt.Printf("Day 2.2: %d\n", horizontal*depth)
 }
