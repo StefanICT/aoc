4e4ce58ef3aa2a4f312de5c077775d5dbb1c0c0e
Stefan van der Wolf <stefan@dexels.com>
2021-12-06 16:37:57 +0100

    Solve day 5

--- 2021/5/main.go
+++ 2021/5/main.go
@@ -1,23 +1,26 @@
 package main
 
 import (
-    . "aoc/utils"
-    "regexp"
+	. "aoc/utils"
 	"bufio"
 	"fmt"
 	"os"
+	"regexp"
 )
 
 type Line struct {
-    start Point
-    end Point
+	start Point
+	end   Point
+}
+
+func (line Line) isDiagonal() bool {
+	return line.start.x != line.end.x && line.start.y != line.end.y
 }
 
 type Point struct {
-    x int
-    y int
+	x int
+	y int
 }
-
 
 func main() {
 	part1()
@@ -25,75 +28,134 @@ func main() {
 }
 
 func part1() {
-	file, _ := os.Open("5/test")
-	defer file.Close()
+	lines := makeLines()
+	grid := makeGrid(max(lines))
 
-    re := regexp.MustCompile(`(\d+),(\d)\s+->\s+(\d),(\d)`)
+	for _, line := range lines {
+		if line.isDiagonal() {
+			continue
+		}
 
-    lines := []Line{}
-
-	scanner := bufio.NewScanner(file)
-	for scanner.Scan() {
-        match := re.FindStringSubmatch(scanner.Text())
-
-        line := Line{
-            start: Point {
-                x: AsInt(match[1]),
-                y: AsInt(match[2]),
-            },
-            end: Point {
-                x: AsInt(match[3]),
-                y: AsInt(match[4]),
-            },
-        }
-        lines = append(lines, line)
-
+		plot(line, grid)
 	}
 
-    fmt.Println(lines)
-    fmt.Println(max(lines))
-    debug(grid(max(lines)))
-
-    fmt.Println("Day 5")
+	fmt.Println("Day 5.1:", count(grid, 2))
 }
 
 func part2() {
+	lines := makeLines()
+	grid := makeGrid(max(lines))
+
+	for _, line := range lines {
+		plot(line, grid)
+	}
+
+	fmt.Println("Day 5.1:", count(grid, 2))
 }
 
-// Grid:
-// >> X
-// \/ Y
-func grid(max Point) [][]int {
-    grid := make([][]int, max.y + 1) 
+func makeLines() []Line {
+	file, _ := os.Open("5/input")
+	defer file.Close()
 
-    for i := 0; i < max.x; i++ {
-        grid = append(grid, make([]int, max.x))
-    }
+	re := regexp.MustCompile(`(\d+),(\d+)\s+->\s+(\d+),(\d+)`)
 
-    return grid
+	lines := []Line{}
+
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		match := re.FindStringSubmatch(scanner.Text())
+
+		line := Line{
+			start: Point{
+				x: AsInt(match[1]),
+				y: AsInt(match[2]),
+			},
+			end: Point{
+				x: AsInt(match[3]),
+				y: AsInt(match[4]),
+			},
+		}
+		lines = append(lines, line)
+
+	}
+
+	return lines
+}
+
+func makeGrid(max Point) [][]int {
+	grid := make([][]int, max.x+1)
+
+	for i := 0; i < max.y+1; i++ {
+		grid[i] = make([]int, max.x+1)
+	}
+
+	return grid
 }
 
 func max(lines []Line) Point {
-    x := 0
-    y := 0
+	x := 0
+	y := 0
 
-    for _, line := range lines {
-        x = Max(x, line.start.x) 
-        y = Max(y, line.start.y) 
+	for _, line := range lines {
+		x = Max(x, line.start.x)
+		y = Max(y, line.start.y)
 
-        x = Max(x, line.end.x) 
-        y = Max(y, line.end.y) 
-    }
+		x = Max(x, line.end.x)
+		y = Max(y, line.end.y)
+	}
 
-    return Point{ x, y }
+	return Point{x, y}
 }
 
-func debug(grid [][]int) {
-    fmt.Print("Grid:")
-    for _, row := range grid {
-        for _, column := range row {
-            fmt.Print(column)
-        }
-        fmt.Print("\n")
-    }
+func plot(line Line, grid [][]int) {
+	// Use Bresenham's line algorithm
+    // https://medium.com/geekculture/bresenhams-line-drawing-algorithm-2e0e953901b3
+
+    dx := Abs(line.end.x - line.start.x)
+    dy := Abs(line.end.y - line.start.y)
+
+	var sx int
+	if line.start.x < line.end.x {
+		sx = 1
+	} else {
+		sx = -1
+	}
+
+	var sy int
+	if line.start.y < line.end.y {
+		sy = 1
+	} else {
+		sy = -1
+	}
+
+    d := dx - dy
+
+	for {
+		grid[line.start.y][line.start.x] += 1
+		if line.start == line.end {
+			return
+		}
+
+        d2 := 2 * d
+		if d2 > (0 - dy) {
+			d -= dy
+			line.start.x += sx
+		}
+		if d2 < dx {
+			d += dx
+			line.start.y += sy
+		}
+	}
+}
+
+func count(grid [][]int, value int) int {
+	sum := 0
+	for _, row := range grid {
+		for _, column := range row {
+			if column >= value {
+				sum += 1
+			}
+		}
+	}
+	return sum
 }
--- 2021/utils/utils.go
+++ 2021/utils/utils.go
@@ -1,23 +1,30 @@
 package utils
 
 import (
-    "strconv"
+	"strconv"
 )
 
-func Memset(ints *[]int, value int) {
-    for i := 0; i < len(*ints); i++ {
-        (*ints)[i] = value
+func Abs(i int) int {
+    if i < 0 {
+        return -i
     }
+    return i
+}
+
+func Memset(ints *[]int, value int) {
+	for i := 0; i < len(*ints); i++ {
+		(*ints)[i] = value
+	}
 }
 
 func Max(x int, y int) int {
-    if x < y {
-        return y
-    }
-    return x
+	if x < y {
+		return y
+	}
+	return x
 }
 
 func AsInt(s string) int {
-    n, _ := strconv.Atoi(s)
-    return n
+	n, _ := strconv.Atoi(s)
+	return n
 }
